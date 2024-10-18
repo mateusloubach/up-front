27bcd5665b6ce429f0b5675b006f91583a385e0a
loubachmateus <mateuscesardev@gmail.com>
2024-10-18 12:53:55 -0300

    feat: add react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export { };
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+
+            <Route path="/home" element={<p>página inicial</p>} />
+
+            <Route path="*" element={<Navigate to={"/home"} />} />
+        </Routes>
+    );
+}
