2e06ebb521b1d891b668d6c1b260c8087f1bf9bf
loubachmateus <mateuscesardev@gmail.com>
2024-10-18 14:15:10 -0300

    feat: add mui; theme;

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,15 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
     return (
         <Routes>
 
-            <Route path="/home" element={<p>página inicial</p>} />
+            <Route path="/home" element={
+                <Button variant="contained" color="primary">
+                    Home
+                </Button>
+            } />
 
             <Route path="*" element={<Navigate to={"/home"} />} />
         </Routes>
